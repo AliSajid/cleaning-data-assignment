5b96bc96574f1c8ce979d9d6ddbbf7bed65a98ef
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2015-07-27 02:20:32 +0500

    Make it write a .txt file and fixed the step numbers.

--- run_analysis.R
+++ run_analysis.R
@@ -27,13 +27,13 @@ train.data <- read.table("./UCI HAR Dataset/train/X_train.txt", header=FALSE)
 train.data.act <- read.table("./UCI HAR Dataset/train/y_train.txt", header=FALSE)
 train.data.sub <- read.table("./UCI HAR Dataset/train/subject_train.txt", header=FALSE)
 
-# 2. Uses descriptive activity names to name the activities in the data set
+# 1. Uses descriptive activity names to name the activities in the data set
 
 activities <- read.table("./UCI HAR Dataset/activity_labels.txt", header=FALSE, colClasses="character")
 test.data.act$V1 <- factor(test.data.act$V1, levels=activities$V1, labels=activities$V2)
 train.data.act$V1 <- factor(train.data.act$V1, levels=activities$V1, labels=activities$V2)
 
-# 3. Appropriately labels the data set with descriptive activity names
+# 2. Appropriately labels the data set with descriptive activity names
 
 features <- read.table("./UCI HAR Dataset/features.txt", header=FALSE, colClasses="character")
 colnames(test.data) <- features$V2
@@ -43,18 +43,18 @@ colnames(train.data.act) <- c("Activity")
 colnames(test.data.sub) <- c("Subject")
 colnames(train.data.sub) <- c("Subject")
 
-# 4. merge test and training sets into one data set, including the activities
+# 3. merge test and training sets into one data set, including the activities
 test.data <- cbind(test.data, test.data.act)
 test.data <- cbind(test.data, test.data.sub)
 train.data <- cbind(train.data, train.data.act)
 train.data <- cbind(train.data, train.data.sub)
 big.data <- rbind(test.data, train.data)
 
-# 5. extract only the measurements on the mean and standard deviation for each measurement
+# 4. extract only the measurements on the mean and standard deviation for each measurement
 big.data.mean <- sapply(big.data, mean, na.rm=TRUE)
 big.data.sd <- sapply(big.data, sd, na.rm=TRUE)
 
 # 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 DT <- data.table(big.data)
 tidy <- DT[, lapply(.SD, mean), by="Activity,Subject"]
-write.table(tidy, file="tidy.csv", sep=",", row.names = FALSE)
+write.table(tidy, file="tidy.txt", sep=",", row.names = FALSE)
